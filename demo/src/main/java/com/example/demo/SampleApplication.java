b25d8b25da377c55970e38cb47f03eb81523c82e
Rujun Chen <rujche@microsoft.com>
2020-05-13 16:53:53 +0800

    Update sample. Check whether problem fixed. (https://github.com/microsoft/azure-spring-boot/issues/637)

---
+++ demo/src/main/java/com/example/demo/SampleApplication.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SampleApplication implements CommandLineRunner {
+
+    @Value("${yourSecretPropertyName9}")
+    private String mySecretProperty;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+    public void run(String[] args) {
+        log.info("property yourSecretPropertyName9 in Azure Key Vault: {}", mySecretProperty);
+    }
+
+}
