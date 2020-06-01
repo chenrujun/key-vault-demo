64ec987b56e14edc4670bfb8520fd2d8fb1da792
Rujun Chen <rujche@microsoft.com>
2020-06-01 14:59:47 +0800

    Test configuration class.

--- demo/src/main/java/com/example/demo/SampleApplication.java
+++ demo/src/main/java/com/example/demo/SampleApplication.java
@@ -7,17 +7,23 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @Slf4j
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SampleApplication implements CommandLineRunner {
 
     @Value("${yourSecretPropertyName}")
     private String yourSecretPropertyName;
+
+    @Autowired
+    KeyVaultTestProperties keyVaultTestProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
@@ -25,6 +31,8 @@ public class SampleApplication implements CommandLineRunner {
 
     public void run(String[] args) {
         log.info("property yourSecretPropertyName in Azure Key Vault: {}", yourSecretPropertyName);
+        log.info("keyVaultTestProperties.getTestProperty(): {}", keyVaultTestProperties.getTestProperty());
+
     }
 
 }
